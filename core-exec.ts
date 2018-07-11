229e37f57def0a3563f077c1e18ad2174e80c35a
William Temple <William.Temple@colorado.edu>
2018-07-11 09:40:16 -0600

    first commit
    Signed-off-by: William Temple <William.Temple@colorado.edu>

---
+++ core-exec.ts
@@ -0,0 +1,40 @@
+namespace _pxexec {
+	interface EventDescription {
+		predicate: () => boolean,
+		handler: () => void
+	}
+
+	var forever_functions : Array<() => void>;
+
+	var events : { [k : string] : EventDescription };
+
+	export function init() {
+		forever_functions = [];
+		events = {};
+	}
+
+	export function add_forever(func : () => void) : void {
+		forever_functions.push(func);
+	}
+	
+	export function register_event(eventId : string, event : EventDescription) {
+		events[eventId] = event;
+	}
+
+    export function run() : void {
+
+		while (true) {
+			for (let i = 0; i < forever_functions.length; i++) {
+				forever_functions[i]();
+			}
+			for (let key in events) {
+				const ev = events[key];
+				if (ev.predicate()) {
+					ev.handler();
+				}
+			}
+		}
+    }
+}
+
+export default _pxexec;

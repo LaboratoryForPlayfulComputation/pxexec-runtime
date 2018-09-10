bc02fe4feec825f72bd1e4890b72706ce7adcf52
William Temple <William.Temple@colorado.edu>
2018-09-10 10:27:38 -0600

    WIP stubbing out some basic UDP signaling
    Signed-off-by: William Temple <William.Temple@colorado.edu>

--- lib/network.ts
+++ lib/network.ts
@@ -1,5 +1,10 @@
 import { ChildProcess, spawn } from 'child_process';
+import { Socket, createSocket } from 'dgram';
 import { EventEmitter } from 'events';
+
+import * as readline from 'readline';
+
+import * as uuidv4 from 'uuid/v4';
 
 namespace network {
 
@@ -9,22 +14,71 @@ namespace network {
 
 	const _eventDispatcher: EventEmitter = new EventEmitter();
 
+	namespace signaling {
+		const _UDP_SIGNALING_PORT = 38714;
+		const _UDP_BROADCAST_ADDR = "255.255.255.255";
+		interface UDPSignalingOffer {
+			sourceid: string,
+			targetid: string,
+			encodedSDP: string,
+		}
+
+		let _socket: Socket | undefined = undefined;
+
+		export function enableUDPSignaling(): void {
+			_socket = createSocket('udp4');
+
+			_socket.bind(_UDP_SIGNALING_PORT, function () {
+				_socket.setBroadcast(true);
+			})
+
+			_socket.on("message", (msg, rinfo) => {
+				console.log(`dgram in: "${msg.toString()}" from ${rinfo.address}:${rinfo.port}`)
+			})
+		}
+
+		export function broadcastOffer(other: string, sdp: string | Buffer) {
+			const msg: UDPSignalingOffer = {
+				sourceid: _id,
+				targetid: other,
+				encodedSDP: sdp.toString()
+			};
+			_socket.send(JSON.stringify(msg), _UDP_SIGNALING_PORT, _UDP_BROADCAST_ADDR, (err, bytes) => {
+				console.log(`callback from send: ${err}, ${bytes}`);
+			});
+		}
+	}
+
+	/**
+	 * The base format of all messages FROM Python to this handler.
+	 */
 	interface RPCMarshallingFormat {
 		type: "event" | "response",
 		value: RPCResponse | RPCEvent,
 	}
 
+	/**
+	 * The format of a Reponse sub-message, indicating a return value from
+	 * a function call.
+	 */
 	interface RPCResponse {
 		id: number,
 		rpcEndpoint: string,
 		value: any,
 	}
 
+	/**
+	 * The format of an Event sub-message, indicating an event that was raised
+	 * asynchronously in the Python daemon.
+	 */
 	interface RPCEvent {
 		name: string,
 		data: any
 	}
 
+	/**
+	 * The format of an RPC invocation from this handler TO the remote daemon.
+	 */
 	interface PythonRPCCall {
 		id: number,
 		rpcEndpoint: string,
@@ -34,13 +88,21 @@ namespace network {
 
 	var _DAEMON_CHILD: ChildProcess | undefined = undefined;
 
+	var _id: string | undefined = undefined;
+
+	/**
+	 * Interpret and dispatch data returned to this handler by the underlying
+	 * Python daemon.
+	 * @param dataBuf the string to interpret
+	 */
 	function _unMarshalRPCData(dataBuf: string | Buffer) {
+		const dataStr = dataBuf.toString();
+		console.log(dataStr);
 		const data: RPCMarshallingFormat = <RPCMarshallingFormat>JSON.parse(dataBuf.toString());
 		const type = data.type;
 		switch (type) {
 			case "event":
 				const event = <RPCEvent>data.value;
-				console.log(`got an event: ${event}`);
 				_eventDispatcher.emit(event.name, event.data)
 				break;
 			case "response":
@@ -54,7 +116,15 @@ namespace network {
 		}
 	}
 
-	export function initialize() {
+	/**
+	 * Initialize the network. This function creates a python daemon to handle
+	 * RTC remotely. We call that daemon using an stdio-based RPC mechanism that
+	 * uses JSON as a marshalling format.
+	 * 
+	 * @param peerid optionally, the universally unique identifier of your connection
+	 */
+	export function initialize(peerid: string) {
+		_id = peerid;
 		_DAEMON_CHILD = spawn(PYTHON, ['-u', '-m', 'node-aiortc-daemon']);
 		_DAEMON_CHILD.stdout.on('data', _unMarshalRPCData);
 		_DAEMON_CHILD.stderr.on('data', (data) => {
@@ -64,13 +134,25 @@ namespace network {
 			console.log(`RTC daemon closed with status ${code} `);
 		});
 		console.log(`RTC Daemon created and initialized: ${_DAEMON_CHILD} `);
+
+		signaling.enableUDPSignaling();
 	}
 
-	export function foo(cb?: (n: number) => void): void {
-		_send_call("foo", cb);
+	/**
+	 * Return the configured Peer ID.
+	 */
+	export function getId(): string {
+		return _id;
 	}
 
-	export function offer(cb?: (s: string) => void): void {
+	export function offer_to(otherpeer: string) {
+		offer((sdp) => {
+			console.log(sdp);
+			signaling.broadcastOffer(otherpeer, sdp);
+		})
+	}
+
+	function offer(cb?: (s: string) => void): void {
 		_send_call("get_local_offer_sdp", cb);
 	}
 
@@ -86,6 +168,15 @@ namespace network {
 		_eventDispatcher.addListener("channel_message", cb);
 	}
 
+	/**
+	 * 
+	 * @param endpoint the name of the remote function to call
+	 * @param cb a callback to invoke when the function returns, with the return
+	 * value passed as an argument
+	 * @param args optional set of arguments to use on the remote end (must match the arity and type constraints given
+	 * in the remote program)
+	 * @param kwArgs optional dictionary of kwArgs, passed as keyword arguments on the remote end
+	 */
 	function _send_call(
 		endpoint: string,
 		cb?: (...args: any[]) => void,

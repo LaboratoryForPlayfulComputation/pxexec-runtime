d7f4f418b3ea5cebf42c04e56273273831138336
Tanner Quigley <TannerQuigley@me.com>
2019-01-31 12:24:30 -0700

    reponse to Will's comments. Added append and read as well as create sheet factory function
    Signed-off-by: Tanner Quigley <TannerQuigley@me.com>

--- lib/sheets.ts
+++ lib/sheets.ts
@@ -1,14 +1,16 @@
 import * as fs from "fs";
 import { env } from "./core-exec";
 
-// Google stuff
-import google = require('googleapis');
+import { google } from 'googleapis';
+
+import { log } from './console';
 
 
 let token: any;
 let credentials: any;
 
 let oAuth2Client: any;
+let sheets: any;
 
 // parse token.js and credentials.js
 export function initialize() {
@@ -23,6 +25,9 @@ export function initialize() {
 
     const { client_secret, client_id, redirect_uris } = credentials.installed;
     oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+    oAuth2Client.setCredentials(token);
+
+    sheets = google.sheets({ version: 'v4', oAuth2Client });
 
     return;
 }
@@ -33,7 +38,7 @@ export function initialize() {
 //   Read Row
 //   Read Cell
 
-export class Spreadheet {
+export class Spreadsheet {
 
     private spreadsheetID: string;
     private name: string;
@@ -43,33 +48,104 @@ export class Spreadheet {
         this.name = name;
     }
 
-    public clearSheet() {
-        const sheets = google.sheets({ version: 'v4', oAuth2Client });
-
+    public clear() {
         sheets.spreadsheets.values.clear({
-            range: name,
+            range: this.name,
             spreadsheetId: this.spreadsheetID,
-        }, (err, result) => {
+        }, (err: any, result: any) => {
             if (err) {
                 // Handle error.
-                console.log(err);
+                // console.log(err);
             } else {
-                console.log(`${result.data} cells cleared.`);
+                // console.log(`${result.data} cells cleared.`);
             }
         });
     }
 
+    public readRow(row: number) {
+        sheets.spreadsheets.values.get({
+            range: this.name + "!" + row + ":" + row,
+            spreadsheetId: this.spreadsheetID,
+        }, (err: any, res: any) => {
+            if (err) {
+                return log('The API returned an error: ' + err);
+            }
+            const rows = res.data.values;
+            if (rows.length) {
+                return rows;
+            } else {
+                log('No data found.');
+            }
+        });
+    }
+
+    public readCell(cell: string) {
+        sheets.spreadsheets.values.get({
+            range: this.name + "!" + cell,
+            spreadsheetId: this.spreadsheetID,
+        }, (err: any, res: any) => {
+            if (err) {
+                return log('The API returned an error: ' + err);
+            }
+            const rows = res.data.values;
+            if (rows.length) {
+                return rows;
+            } else {
+                log('No data found.');
+            }
+        });
+    }
+
+    public appendRow(row: string[]) {
+        const values = row;
+        const resource = {
+            values,
+        };
+        sheets.spreadsheets.values.append({
+            range: this.name,
+            resource,
+            spreadsheetId: this.spreadsheetID,
+            valueInputOption: 'RAW',
+        }, (err: any, result: any) => {
+            if (err) {
+                // Handle error.
+                log(err);
+            } else {
+                log(`${result.data.updates.updatedCells} cells appended.`);
+            }
+        });
+    }
 }
 
 
-// Factory Method to create spreadsheet by name, return spreadsheet class
-/*
+// Factory method to create spreadsheet by name, return spreadsheet class
 export function createSheet(name: string) {
-    return new Spreadsheet()
+
+    let newID: string;
+
+    const resource = {
+        properties: {
+            title: name
+        },
+    };
+
+    sheets.spreadsheets.create({
+        fields: 'spreadsheetId',
+        resource,
+    }, (err: any, spreadsheet: any) => {
+        if (err) {
+            // Handle error.
+            log(err);
+        } else {
+            log(spreadsheet.data.spreadsheetId)
+            newID = spreadsheet.data.spreadsheetId;
+        }
+    });
+
+    return new Spreadsheet(newID, name);
 }
 
 // Factory method to get spreadsheet from ID, return spreadsheet class
-export function getSheet(id: string) {
-
+export function getSheet(id: string, name: string) {
+    return new Spreadsheet(id, name);
 }
-*/

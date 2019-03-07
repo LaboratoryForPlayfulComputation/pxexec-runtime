a4838c686989513a1f08a74c7d54519c8301fb51
shadowfax6894 <anniekelly6894@gmail.com>
2019-03-06 18:11:02 -0700

    adding typings for dmx stuff

---
+++ dmx.d.ts
@@ -0,0 +1,13 @@
+
+declare module 'dmx' {
+    namespace DMX {
+        class DMX {
+            constructor();
+            public registerDriver(name: string, module: string): void;
+            public addUniverse(name: string, driver: string, deviceId?: string, options?: any): void;
+            public update(universe: string, channels: Object): void;
+            public updateAll(universe: string, value: number): void;
+            public universeToObject(universeKey: string): Object;
+        }        
+    }
+}
--- lib/dmx.ts
+++ lib/dmx.ts
@@ -1,10 +1,17 @@
-import { dmx } from 'node-dmx';
+import { DMX } from 'dmx';
 
 import { log } from './console';
 
 import { _await } from './core-exec';
 
 export function initialize() {
-    log(`hi`)
+    const dmx = new DMX.DMX();
+    //const universe = dmx.addUniverse('test', 'dmxking-ultra-dmx-pro', '/dev/ttyUSB0');
+    const universe = dmx.addUniverse('test', 'null');
+    log(universe);
     return;
+}
+
+// Method to create new DMX fixture
+export function createFixture(): void {
 }

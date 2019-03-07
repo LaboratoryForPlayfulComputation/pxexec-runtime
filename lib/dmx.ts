4c04c08ab5fb7141ebd08de206d920e56f372551
shadowfax6894 <anniekelly6894@gmail.com>
2019-03-06 21:10:37 -0700

    adding logic for updating dmx channels

--- lib/dmx.ts
+++ lib/dmx.ts
@@ -4,26 +4,9 @@ import { log } from './console';
 
 import { _await } from './core-exec';
 
-enum Colors {
-    //% block=red
-    Red = 0xFF0000,
-    //% block=orange
-    Orange = 0xFFA500,
-    //% block=yellow
-    Yellow = 0xFFFF00,
-    //% block=green
-    Green = 0x00FF00,
-    //% block=blue
-    Blue = 0x0000FF,
-    //% block=purple
-    Purple = 0xFF00FF,
-    //% block=white
-    White = 0xFFFFFF,
-    //% block=black
-    Black = 0x000000
-}
-
-let allFixtures : Array<Fixture> = [];
+let allFixtures  : Array<Fixture> = [];
+let universeName : string = 'pidmx';
+let dmx : DMX.DMX;
 
 export class Fixture {
     public name : string;
@@ -60,9 +43,9 @@ export class Channel {
 }
 
 export function initialize() {
-    const dmx = new DMX.DMX();
-    //const universe = dmx.addUniverse('test', 'dmxking-ultra-dmx-pro', '/dev/ttyUSB0');
-    const universe = dmx.addUniverse('test', 'null');
+    dmx = new DMX.DMX();
+    //const universe = dmx.addUniverse(universeName, 'dmxking-ultra-dmx-pro', '/dev/ttyUSB0');
+    const universe = dmx.addUniverse(universeName, 'null');
     log(universe);
     return;
 }
@@ -74,14 +57,11 @@ export function createFixture(name: string, numChannels: number): void {
 
 export function updateFixtureChannel(name: string, channel: number, value: number) { 
     let fixture = findFixtureByName(name);
-    if (fixture) {
-        fixture.channels[channel].value = value;
-        // update dmx
-    }
+    if (fixture) fixture.channels[channel].value = value;
 }
 
 export function send(): void {
-    // send dmx
+    dmx.update(universeName, generateDMXJson());
 }
 
 export function findFixtureByName(name: string) : Fixture {
@@ -92,4 +72,17 @@ export function findFixtureByName(name: string) : Fixture {
         }
     }
     return null;
+}
+
+export function generateDMXJson(): any {
+    let dmxChannels : any = {};
+    let channelCount = 1;
+    for (let i = 0; i < allFixtures.length; i++) {
+        let fixture = allFixtures[i];
+        for (let j = 0; j < fixture.numChannels; j++) {
+            let index : number = channelCount + j;
+            dmxChannels[index as number] = fixture.channels[j];
+        }
+    }
+    return dmxChannels;
 }
